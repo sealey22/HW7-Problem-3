d7b530fccc32cd09e0d6b5f8d8a03aab9947a855
Hsealey <112498796+sealey22@users.noreply.github.com>
2022-10-12 00:49:31 +0000

    transferred from books

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
+/*5. (Desk  check)  Determine  the  values  displayed  by  each  cout  statement  in  the  following   program: */
+
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
-}
+using namespace std;
+
+int firstnum = 10;  // declare and initialize a global variable
+
+void display(); //function prototype
+
+int main(){  
+  
+  int firstnum = 20;   // declare and initialize a local variable  
+  
+  cout << "\nThe value of firstnum is " << firstnum << endl; 
+  
+  display();  
+  
+  return 0;
+  }
+
+void display(void) {  
+  cout << "The value of firstnum is now " << firstnum << endl;  
+  
+  return;
+  }
+// 
+it will first spit out " the value of the firstnum is 20 " because that is the only input that is called in the main fucntino before the user function diplay(void) and that will spit out the value of firstnum is now 10. This is because this function is called in after the int firstnum = 20.
+//
